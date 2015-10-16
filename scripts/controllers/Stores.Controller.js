e9ec5a847143b1e71a89e3b376ef5ca879b247f6
Luke Lancaster <luketlancaster@gmail.com>
2015-10-16 14:51:47 -0500

    base layout of page and redirect if google undefined

--- scripts/controllers/Stores.Controller.js
+++ scripts/controllers/Stores.Controller.js
@@ -2,20 +2,25 @@ angular
   .module('TacoYelp')
   .controller('StoresCtrl', StoresCtrl);
 
-function StoresCtrl($scope, $routeParams, $http) {
+function StoresCtrl($location, $scope, $routeParams, $http) {
   var vm = this;
 
-  var request = {
-    placeId: $routeParams.id
-  }
-
-  var service = new google.maps.places.PlacesService(map);
-
-  service.getDetails(request, function(place, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      vm.place = place;
-      console.log(place);
-      $scope.$apply();
+  if (typeof google === 'undefined') {
+    window.location = '/'
+  } else {
+    console.log('yes')
+    var request = {
+      placeId: $routeParams.id
     }
-  })
+
+    var service = new google.maps.places.PlacesService(map);
+
+    service.getDetails(request, function(place, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        vm.place = place;
+        console.log(place);
+        $scope.$apply();
+      }
+    })
+  }
 }
